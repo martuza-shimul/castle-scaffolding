adadaf8213fec035554253d4a3c074ce13739764
Martuza <martuza.shimul@gmail.com>
2020-01-03 22:24:45 +0600

    php files configured

--- dist/php/data.php
+++ dist/php/data.php
@@ -41,10 +41,10 @@ $message = $_POST['message'];
 
 $to = 'tripkeys@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Quote Engine:  $first_name";
-$email_body = "You have received a new message from your Painters London website Quote Engine .\n\n"."Here are the details:\n\nName: $first_name\n\nPhone: $phone\n\nEmail: $email\n\n  Postcode: $postcode \n\n Message: $message
+$email_body = "You have received a new message from your Scaffolding London website Quote Engine .\n\n"."Here are the details:\n\nName: $first_name\n\nPhone: $phone\n\nEmail: $email\n\n  Postcode: $postcode \n\n Message: $message
 
 \n\n\n Here is the Quote Request: \n Type of Work: $first\n  Area: $area \n Timeline: $third \n  Material Purchase: $material ";
-$headers = "From: noreply@painters-london.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@scaffolding.co.uk\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 
 $headers .= "Reply-To: $email";	
 mail($to,$email_subject,$email_body,$headers);
